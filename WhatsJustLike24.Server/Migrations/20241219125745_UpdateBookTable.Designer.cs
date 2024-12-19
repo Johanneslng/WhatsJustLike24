bb2c4fc1284ea6990cf4a93023340978b6e0409b
Johanneslng <86969918+Johanneslng@users.noreply.github.com>
2024-12-19 14:05:49 +0100

    updated db

--- WhatsJustLike24.Server/Migrations/20241219125745_UpdateBookTable.Designer.cs
+++ WhatsJustLike24.Server/Migrations/20241219125745_UpdateBookTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WhatsJustLike24.Server.Data;
 
@@ -11,9 +12,11 @@ using WhatsJustLike24.Server.Data;
 namespace WhatsJustLike24.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241219125745_UpdateBookTable")]
+    partial class UpdateBookTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WhatsJustLike24.Server/Migrations/20241219125745_UpdateBookTable.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhatsJustLike24.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBookTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "Isbn",
+                table: "BookDetails",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "Isbn13",
+                table: "BookDetails",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Publisher",
+                table: "BookDetails",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Series",
+                table: "BookDetails",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Isbn",
+                table: "BookDetails");
+
+            migrationBuilder.DropColumn(
+                name: "Isbn13",
+                table: "BookDetails");
+
+            migrationBuilder.DropColumn(
+                name: "Publisher",
+                table: "BookDetails");
+
+            migrationBuilder.DropColumn(
+                name: "Series",
+                table: "BookDetails");
+        }
+    }
+}
